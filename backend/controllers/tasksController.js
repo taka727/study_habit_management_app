d2c33da48a54d1381c313aa10d03b9e0f97211bf
taka727 <taka_10727@icloud.com>
2025-05-09 00:00:22 +0900

    feat : APIのひな形作成

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,1 +1,21 @@
 const prisma = require('../prismaClient');
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await prisma.task.findMany({
+            include: {
+                user: true,
+                status: true,
+                subtasks: true,
+            },
+        });
+        res.json({ status: 'success', data: tasks });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', message: 'サーバーエラー' });
+    }
+};
+
+module.exports = {
+    getAllTasks
+};
