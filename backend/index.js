0f68b7f5886080cef643f609dd00ca1bd77ce7fe
taka727 <taka_10727@icloud.com>
2025-05-08 23:19:47 +0900

    feat : indexにルーティング統合

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
+const taskRoutes = require("./routers/tasksRouter");
 
 const app = express();
 const port = 3000;
 const prisma = require('./prismaClient');
+
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -20,6 +23,8 @@ db.connect(err => {
         console.log('Connected to MySQL');
     }
 });
+
+app.use('/tasks',taskRoutes);
 
 app.get('/', (req, res) => {
     console.log('start getmethod');
